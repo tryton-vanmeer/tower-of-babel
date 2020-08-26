28af09588077ac10aeb23b454c9388d2f8c20bcb
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-08-26 15:15:22 -0400

    Add JavaScript example for FizzBuzz.

---
+++ FizzBuzz/javascript/fizzbuzz.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+for (const i of Array(101).keys())
+{
+    if ((i % 15) === 0)
+    {
+        console.log("FizzBuzz");
+    }
+    else if ((i % 3) === 0)
+    {
+        console.log("Fizz");
+    }
+    else if ((i % 5) === 0)
+    {
+        console.log("Buzz");
+    }
+    else
+    {
+        console.log(i);
+    }
+}
