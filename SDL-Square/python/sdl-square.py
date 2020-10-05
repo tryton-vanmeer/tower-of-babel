c0f7eee603163e9b5aa5df8c5738bf12bdb215c2
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-10-05 15:12:33 -0400

    Render red square in center.

--- SDL-Square/python/sdl-square.py
+++ SDL-Square/python/sdl-square.py
@@ -9,5 +9,14 @@ sdl2.ext.init()
 window = sdl2.ext.Window("SDL Square", size=(800, 600))
 window.show()
 
+factory = sdl2.ext.SpriteFactory(sdl2.ext.SOFTWARE)
+square = factory.from_color(sdl2.ext.Color(255, 0, 0), size=(256, 256))
+square.position = (400 - 128), (300 - 128)
+
+spriterenderer = factory.create_sprite_render_system(window)
+spriterenderer.render(square)
+
 processor = sdl2.ext.TestEventProcessor()
 processor.run(window)
+
+sdl2.ext.quit()
