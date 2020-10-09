6df48696a414319005f964b229e6d8f444e78176
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-10-09 13:50:42 -0400

    Add Rust example for GTK-Hello-World.

--- GTK-Hello-World/rust/src/main.rs
+++ GTK-Hello-World/rust/src/main.rs
@@ -1,25 +1,55 @@
 extern crate gtk;
-extern crate gio;
 
 use gtk::prelude::*;
-use gio::prelude::*;
 
-use gtk::{Application, ApplicationWindow};
+use std::{process};
 
-fn main()
+pub struct GtkHelloWorld
 {
-    let application = Application::new(
-        Some("io.github.tryton-vanmeer.tower-of-babel.gtk-hello-world"),
-        Default::default(),
-    ).expect("Failed to initialize GTK application");
+    window: gtk::Window
+}
 
-    application.connect_activate(|app| {
-        let window = ApplicationWindow::new(app);
+impl GtkHelloWorld
+{
+    pub fn new() -> Self
+    {
+        let window = gtk::Window::new(gtk::WindowType::Toplevel);
         window.set_title("GTK Hello World");
         window.set_default_size(400, 300);
 
-        window.show_all();
-    });
+        window.connect_delete_event(move |_, _| {
+            gtk::main_quit();
+            gtk::Inhibit(false)
+        });
 
-    application.run(&[]);
+        // Setup view
+        let button = gtk::Button::with_label("Hello, World");
+        button.connect_clicked(move |button| {
+            button.set_label("Thank you");
+        });
+
+        let hbox = gtk::Box::new(gtk::Orientation::Horizontal, 0);
+        hbox.pack_start(&button, true, false, 0);
+
+        let vbox = gtk::Box::new(gtk::Orientation::Vertical, 0);
+        vbox.pack_start(&hbox, true, false, 0);
+
+        window.add(&vbox);
+
+        GtkHelloWorld { window }
+    }
+}
+
+fn main()
+{
+    if gtk::init().is_err()
+    {
+        eprintln!("failed to initialize GTK Application");
+        process::exit(1);
+    }
+
+    let window = GtkHelloWorld::new();
+    window.window.show_all();
+
+    gtk::main();
 }
