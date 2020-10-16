bfd52816172075469f0b65347d75a347401602f0
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-10-16 11:08:19 -0400

    Add D example for FizzBuzz.

---
+++ FizzBuzz/d/main.d
@@ -0,0 +1,24 @@
+import std.stdio;
+
+void main()
+{
+    foreach (i; 1 .. 101)
+    {
+        if (i % 15 == 0)
+        {
+            writeln("FizzBuzz");
+        }
+        else if (i % 3 == 0)
+        {
+            writeln("Fizz");
+        }
+        else if (i % 5 == 0)
+        {
+            writeln("Buzz");
+        }
+        else
+        {
+            writeln(i);
+        }
+    }
+}
