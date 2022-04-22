8ca3c3e5643a6e178fc9b4696c7ae8f62d881359
Tryton Van Meer <hello@trytonvanmeer.dev>
2022-04-22 17:53:54 -0400

    update formatting.

--- C/src/gtk-4-libadwaita.c
+++ C/src/gtk-4-libadwaita.c
@@ -1,6 +1,6 @@
 #include <adwaita.h>
 
-static void print_hello (GtkWidget *widget, gpointer data) {
+static void print_hello(GtkWidget *widget, gpointer data) {
   g_print("Hello, World!\n");
 }
 
@@ -9,16 +9,16 @@ static void activate(GtkApplication *app) {
   GtkWidget *button = gtk_button_new_with_label("Hello World");
 
   g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
-  g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_window_destroy), window);
+  g_signal_connect_swapped(button, "clicked", G_CALLBACK(gtk_window_destroy), window);
 
   gtk_window_set_title(GTK_WINDOW(window), "Hello");
   gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
   gtk_window_set_child(GTK_WINDOW(window), button);
-  gtk_window_present (GTK_WINDOW (window));
+  gtk_window_present(GTK_WINDOW(window));
 }
 
 int main(int argc, char *argv[]) {
-  g_autoptr (AdwApplication) app = NULL;
+  g_autoptr(AdwApplication) app = NULL;
 
   app = adw_application_new("dev.trytonvanmeer.tower-of-babel.Hello", G_APPLICATION_FLAGS_NONE);
 
