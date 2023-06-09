b064566c43af103d051348ee32f1626e02026775
Tryton Van Meer <code@trytonvanmeer.dev>
2023-06-08 20:45:50 -0400

    add FizzBuzz for javascript.

---
+++ FizzBuzz/javascript/main.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("Fizz");
+    } else if (i % 3 == 0) {
+        console.log("Fizz");
+    } else if (i % 5 == 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
